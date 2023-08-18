eb157b3756ca66049446fcf8e31d8453ae19f95c
Olexii Melnik <alexmelnikk@gmail.com>
2023-08-18 08:59:53 +0300

    commit

---
+++ src/test/java/online/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package online.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
